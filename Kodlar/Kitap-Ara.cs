1708da5d4f53fd6c471ba02dd71261926963f860
tunahan <132030047@ogr.uludag.edu.tr>
2022-05-10 14:27:35 +0300

    lütfen olsun bitsin artık valla gına geldi

---
+++ Kodlar/Kitap-Ara.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kütüphane_Otomasyon
+{
+    public partial class Kitap_Ara : Form
+    {
+        public Kitap_Ara()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Kodlar/Personel_Ana_Ekran.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kütüphane_Otomasyon
+{
+    public partial class Personel_Ana_Ekran : Form
+    {
+        public Personel_Ana_Ekran()
+        {
+            InitializeComponent();
+        }
+
+        private void temizle_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
