92d82e658475d2f7ef1adc011cdffda4def46895
tunahan <132030047@ogr.uludag.edu.tr>
2022-05-10 15:30:19 +0300

    kod eklendi

---
+++ Kodlar/mail_gönder_ekranı.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kütüphane_Otomasyon
+{
+    public partial class mail_gönder_ekranı : Form
+    {
+        public mail_gönder_ekranı()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
