4b28ced6aaf2a520457fa35ddc1c6eba936d9454
tunahan <132030047@ogr.uludag.edu.tr>
2022-06-09 17:22:06 +0300

    bitti

--- Kütüphane Otomasyon/personelEkrani.Designer.cs
+++ Kütüphane Otomasyon/personelEkrani.Designer.cs
@@ -46,6 +46,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.dataGridView5 = new System.Windows.Forms.DataGridView();
+            this.teslimDtp = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
@@ -56,9 +57,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(7, 28);
+            this.button1.Location = new System.Drawing.Point(6, 21);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(134, 56);
+            this.button1.Size = new System.Drawing.Size(117, 42);
             this.button1.TabIndex = 0;
             this.button1.Text = "Üye\r\nKayıt -Sil";
             this.button1.UseVisualStyleBackColor = true;
@@ -66,9 +68,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(7, 89);
+            this.button2.Location = new System.Drawing.Point(6, 67);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(134, 53);
+            this.button2.Size = new System.Drawing.Size(117, 40);
             this.button2.TabIndex = 1;
             this.button2.Text = "Kitap\r\nAlış-Veriş";
             this.button2.UseVisualStyleBackColor = true;
@@ -76,9 +79,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(7, 148);
+            this.button4.Location = new System.Drawing.Point(6, 111);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(134, 52);
+            this.button4.Size = new System.Drawing.Size(117, 39);
             this.button4.TabIndex = 3;
             this.button4.Text = "Kitap\r\nEkle-Sil";
             this.button4.UseVisualStyleBackColor = true;
@@ -86,9 +90,10 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(7, 205);
+            this.button5.Location = new System.Drawing.Point(6, 154);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(134, 60);
+            this.button5.Size = new System.Drawing.Size(117, 45);
             this.button5.TabIndex = 4;
             this.button5.Text = "Ara";
             this.button5.UseVisualStyleBackColor = true;
@@ -96,9 +101,10 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(7, 271);
+            this.button6.Location = new System.Drawing.Point(6, 203);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(134, 47);
+            this.button6.Size = new System.Drawing.Size(117, 35);
             this.button6.TabIndex = 5;
             this.button6.Text = "Kitap Güncelle";
             this.button6.UseVisualStyleBackColor = true;
@@ -106,9 +112,10 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(7, 323);
+            this.button7.Location = new System.Drawing.Point(6, 242);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(134, 33);
+            this.button7.Size = new System.Drawing.Size(117, 25);
             this.button7.TabIndex = 6;
             this.button7.Text = "Rapor-İstatistik";
             this.button7.UseVisualStyleBackColor = true;
@@ -118,9 +125,10 @@
             // 
             this.button8.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.button8.ForeColor = System.Drawing.Color.Red;
-            this.button8.Location = new System.Drawing.Point(7, 361);
+            this.button8.Location = new System.Drawing.Point(6, 271);
+            this.button8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(134, 39);
+            this.button8.Size = new System.Drawing.Size(117, 29);
             this.button8.TabIndex = 7;
             this.button8.Text = "Çıkış";
             this.button8.UseVisualStyleBackColor = false;
@@ -129,12 +137,11 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(171, 44);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Location = new System.Drawing.Point(150, 33);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(543, 172);
+            this.dataGridView1.Size = new System.Drawing.Size(475, 129);
             this.dataGridView1.TabIndex = 8;
             // 
             // groupBox1
@@ -146,11 +153,9 @@
             this.groupBox1.Controls.Add(this.button7);
             this.groupBox1.Controls.Add(this.button5);
             this.groupBox1.Controls.Add(this.button6);
-            this.groupBox1.Location = new System.Drawing.Point(14, 16);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.groupBox1.Size = new System.Drawing.Size(151, 413);
+            this.groupBox1.Size = new System.Drawing.Size(132, 310);
             this.groupBox1.TabIndex = 9;
             this.groupBox1.TabStop = false;
             // 
@@ -158,72 +163,69 @@
             // 
             this.Personeller.AutoSize = true;
             this.Personeller.ForeColor = System.Drawing.Color.Orange;
-            this.Personeller.Location = new System.Drawing.Point(174, 16);
+            this.Personeller.Location = new System.Drawing.Point(152, 12);
             this.Personeller.Name = "Personeller";
-            this.Personeller.Size = new System.Drawing.Size(81, 20);
+            this.Personeller.Size = new System.Drawing.Size(65, 15);
             this.Personeller.TabIndex = 10;
             this.Personeller.Text = "Personeller";
             // 
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(173, 269);
-            this.dataGridView2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView2.Location = new System.Drawing.Point(151, 202);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 51;
             this.dataGridView2.RowTemplate.Height = 25;
-            this.dataGridView2.Size = new System.Drawing.Size(542, 160);
+            this.dataGridView2.Size = new System.Drawing.Size(474, 120);
             this.dataGridView2.TabIndex = 11;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.Orange;
-            this.label1.Location = new System.Drawing.Point(174, 245);
+            this.label1.Location = new System.Drawing.Point(152, 184);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.Size = new System.Drawing.Size(40, 15);
             this.label1.TabIndex = 12;
             this.label1.Text = "Üyeler";
             // 
             // dataGridView3
             // 
             this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView3.Location = new System.Drawing.Point(773, 44);
-            this.dataGridView3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView3.Location = new System.Drawing.Point(676, 33);
             this.dataGridView3.Name = "dataGridView3";
             this.dataGridView3.RowHeadersWidth = 51;
             this.dataGridView3.RowTemplate.Height = 25;
-            this.dataGridView3.Size = new System.Drawing.Size(609, 172);
+            this.dataGridView3.Size = new System.Drawing.Size(533, 129);
             this.dataGridView3.TabIndex = 13;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.Orange;
-            this.label2.Location = new System.Drawing.Point(773, 16);
+            this.label2.Location = new System.Drawing.Point(676, 12);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.Size = new System.Drawing.Size(47, 15);
             this.label2.TabIndex = 14;
             this.label2.Text = "Kitaplar";
             // 
             // dataGridView4
             // 
             this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView4.Location = new System.Drawing.Point(773, 269);
-            this.dataGridView4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView4.Location = new System.Drawing.Point(676, 202);
             this.dataGridView4.Name = "dataGridView4";
             this.dataGridView4.RowHeadersWidth = 51;
             this.dataGridView4.RowTemplate.Height = 25;
-            this.dataGridView4.Size = new System.Drawing.Size(609, 160);
+            this.dataGridView4.Size = new System.Drawing.Size(533, 120);
             this.dataGridView4.TabIndex = 15;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.Color.Orange;
-            this.label3.Location = new System.Drawing.Point(773, 245);
+            this.label3.Location = new System.Drawing.Point(676, 184);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(170, 20);
+            this.label3.Size = new System.Drawing.Size(134, 15);
             this.label3.TabIndex = 16;
             this.label3.Text = "Emanet verilmiş Kitaplar";
             // 
@@ -232,29 +234,37 @@
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.label4.ForeColor = System.Drawing.Color.Red;
-            this.label4.Location = new System.Drawing.Point(14, 433);
+            this.label4.Location = new System.Drawing.Point(12, 325);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(159, 20);
+            this.label4.Size = new System.Drawing.Size(127, 15);
             this.label4.TabIndex = 31;
             this.label4.Text = "GEÇ KALMIŞ KİTAPLAR";
             // 
             // dataGridView5
             // 
             this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView5.Location = new System.Drawing.Point(14, 463);
-            this.dataGridView5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView5.Location = new System.Drawing.Point(12, 347);
             this.dataGridView5.Name = "dataGridView5";
             this.dataGridView5.RowHeadersWidth = 51;
             this.dataGridView5.RowTemplate.Height = 25;
-            this.dataGridView5.Size = new System.Drawing.Size(1368, 252);
+            this.dataGridView5.Size = new System.Drawing.Size(1197, 189);
             this.dataGridView5.TabIndex = 30;
+            // 
+            // teslimDtp
+            // 
+            this.teslimDtp.Location = new System.Drawing.Point(1094, 168);
+            this.teslimDtp.Name = "teslimDtp";
+            this.teslimDtp.Size = new System.Drawing.Size(200, 23);
+            this.teslimDtp.TabIndex = 32;
+            this.teslimDtp.Visible = false;
             // 
             // personelEkrani
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateGray;
-            this.ClientSize = new System.Drawing.Size(1398, 732);
+            this.ClientSize = new System.Drawing.Size(1223, 549);
+            this.Controls.Add(this.teslimDtp);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.dataGridView5);
             this.Controls.Add(this.label3);
@@ -266,6 +276,7 @@
             this.Controls.Add(this.Personeller);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.Name = "personelEkrani";
             this.Load += new System.EventHandler(this.personelEkrani_Load);
@@ -300,5 +311,6 @@
         private Label label3;
         private Label label4;
         private DataGridView dataGridView5;
+        private DateTimePicker teslimDtp;
     }
 }
